8acc6089eb1cab72f3801078f0b7e6d7bf0a6739
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-08-16 15:09:55 -0700

    added reactionSchema

--- Unsolved/models/Reaction.js
+++ Unsolved/models/Reaction.js
@@ -4,35 +4,24 @@ const reactionSchema = new Schema(
   {
     reactionId: {
       type: Schema.Types.ObjectId,
-      default: new Schema.Types.ObjectId
+      default: () => new Types.ObjectId
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280
 
     },
     username: {
       type: String,
       required: true
     },
-    reactions: {
-      
+    createdAt: {
+      type: Date,
+      default: Date.now,
     }
   },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
+  
 );
 
-thoughtSchema
-  .virtual('reactionCount')
-  // Getter
-  .get(function () {
-    return this.reactions.length;
-  });
-
-const Thought = model('thought', thoughtSchema);
-
-module.exports = Thought;
+module.exports = reactionSchema;
--- Unsolved/models/Thought.js
+++ Unsolved/models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('Reaction');
 
 const thoughtSchema = new Schema(
   {
@@ -17,9 +18,10 @@ const thoughtSchema = new Schema(
       type: String,
       required: true
     },
-    reactions: {
+    reactions: [
+      reactionSchema
+    ]
       
-    }
   },
   {
     toJSON: {
